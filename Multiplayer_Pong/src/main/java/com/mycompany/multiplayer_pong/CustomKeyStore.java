95ecc8de0701277d1e93926b116cb3605b3c3f21
@AlexanderArella <arellagirardotalexander@gmail.com>
2021-11-30 20:42:23 -0500

    Added more to CustomKeyStore.java

--- Multiplayer_Pong/src/main/java/com/mycompany/multiplayer_pong/CustomKeyStore.java
+++ Multiplayer_Pong/src/main/java/com/mycompany/multiplayer_pong/CustomKeyStore.java
@@ -11,6 +11,7 @@ import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.security.cert.CertificateException;
+import javax.crypto.KeyGenerator;
 import javax.crypto.SecretKey;
 import javax.crypto.spec.SecretKeySpec;
 
@@ -22,19 +23,19 @@ import javax.crypto.spec.SecretKeySpec;
 public class CustomKeyStore {
 
     private KeyStore ks;
-    private SecretKey secretKey;
     private KeyStore.PasswordProtection passProtection;
     private char[] ksPassword;
+    private FileInputStream fsInput;
 
     /**
-     *  Constructor
+     *  Constructor. 
      * @param password
      * @throws IOException
      */
     public CustomKeyStore(char[] password) throws IOException {
         //Ensure the use of "JavaFX Password Field UI control" or something similar
 
-        FileInputStream fsInput = null;
+        fsInput = null;
         try {
             ks = KeyStore.getInstance("PKCS12");
             fsInput = new FileInputStream("src\\main\\resources\\keystore.p12");
@@ -43,6 +44,7 @@ public class CustomKeyStore {
             e.printStackTrace();
         }
         this.passProtection = new KeyStore.PasswordProtection(password);
+        closeFSInput();
     }
     
     /**
@@ -52,26 +54,49 @@ public class CustomKeyStore {
      * @param algo
      * @return
      */
-    public byte[] computeHash(String message, String algo) throws NoSuchAlgorithmException {
-
-        MessageDigest messageDigest = MessageDigest.getInstance(algo);
+    public byte[] computeHash(String message, String algo) {
+        MessageDigest messageDigest = null;
+        try {
+            messageDigest = MessageDigest.getInstance(algo);
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        }
 
         return messageDigest.digest(message.getBytes(StandardCharsets.UTF_8));
     }
     
+    public void encryptFile(String algo, SecretKey secretKey, )
+    
     /**
-     * 
+     * Creates secret key of type ECDSA with password
      * @param keyPassword
      * @return 
      */
-    public SecretKey createSecretKey(String keyPassword) {
+    public SecretKey generateSecretKeyWithPassword(String keyPassword) {
         
-        this.secretKey = new SecretKeySpec(keyPassword.getBytes(), "ECDSA");
-        return this.secretKey;
+        return new SecretKeySpec(keyPassword.getBytes(), "ECDSA");
     }
     
     /**
-     * 
+     * Generates secret key using AES
+     * @param n should be a value of 128, 192 or 256
+     * @return 
+     */
+    public SecretKey generateSecretKey(int n) {
+        // n 
+        KeyGenerator keyGen = null;
+        try {
+            keyGen = KeyGenerator.getInstance("AES");
+            keyGen.init(n);
+        
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        }
+        return keyGen.generateKey();
+    }
+    
+    /**
+     * Stores KeyStore object in "src\main\resources\keystore.p12"
      * @throws IOException 
      */
     public void storeKeyStore() throws IOException {
@@ -82,6 +107,28 @@ public class CustomKeyStore {
             System.out.println("Key stored");
         } catch (KeyStoreException | FileNotFoundException | NoSuchAlgorithmException
                                    | CertificateException e) {
+            e.printStackTrace();
+        }
+    }
+    
+    public void storeEntry(SecretKey secretKey, String alias) {
+        KeyStore.SecretKeyEntry secretKeyEntry = new KeyStore.SecretKeyEntry(secretKey);
+        try {
+            ks.setEntry(alias, secretKeyEntry, passProtection);
+            System.out.println("Secret key entry stored in the KeyStore.");
+        } catch (KeyStoreException e) {
+            e.printStackTrace();
+        }
+    }
+    
+    /**
+     * Releases resources used by FileInputStream by closing it
+     * Helper method
+     */
+    private void closeFSInput() {
+        try {
+            fsInput.close();
+        } catch (IOException e) {
             e.printStackTrace();
         }
     }
