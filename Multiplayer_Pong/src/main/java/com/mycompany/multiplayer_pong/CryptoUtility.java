084b0dc38f6359a273150d6938e7e734626f3013
@AlexanderArella <arellagirardotalexander@gmail.com>
2021-12-03 20:53:34 -0500

    Improved crypto constructor

--- Multiplayer_Pong/src/main/java/com/mycompany/multiplayer_pong/CryptoUtility.java
+++ Multiplayer_Pong/src/main/java/com/mycompany/multiplayer_pong/CryptoUtility.java
@@ -10,7 +10,9 @@ import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.nio.charset.Charset;
 import java.security.cert.CertificateException;
+import java.util.Arrays;
 import javax.crypto.KeyGenerator;
 import javax.crypto.SecretKey;
 import javax.crypto.spec.SecretKeySpec;
@@ -24,23 +26,32 @@ public class CryptoUtility {
 
     private KeyStore ks;
     private KeyStore.PasswordProtection passProtection;
-    private char[] ksPassword;
+    private char[] ksHashedPassword;
 
     /**
-     *  Constructor for creating a new KeyStore file. 
+     *  Constructor for creating a new KeyStore file with given password. 
      * @param password
      * @throws IOException
      */
     public CryptoUtility(char[] password) throws IOException {
         //Ensure the use of "JavaFX Password Field UI control" or something similar
+        //Ensure string normalization
+        
         FileInputStream fis = null;
         try {
-            if (new File("src\\main\\resources\\keystore.p12").exists()) {
+            if (checkKeyStoreExists()) {
                 fis = new FileInputStream("src\\main\\resources\\keystore.p12");
             }
-            ks.load(fis, password);
+            // Computing hash and storing it in a char[]
+            String tempPass = new String(computeHash(password.toString()));
+            ksHashedPassword = computeHash(tempPass);
+            
+            ks.load(fis, ksHashedPassword);
+            System.out.println("KeyStore file created at \"src\\main\\resources\\keystore.p12\"");
         } catch (NoSuchAlgorithmException | CertificateException e) {
             e.printStackTrace();
+        } finally {
+            fis.close();
         }
         FileOutputStream fos = null;
         try {
@@ -49,29 +60,49 @@ public class CryptoUtility {
         } catch (IOException | KeyStoreException | NoSuchAlgorithmException 
                                                  | CertificateException e) {
             e.printStackTrace();
+        } finally {
+            fos.close();
         }
     }
     
-    private boolean checkKeyStore() {
+    /**
+     * Checks if a keystore.p12 file exists
+     * @return 
+     */
+    private boolean checkKeyStoreExists() {
         return new File("src\\main\\resources\\keystore.p12").exists();
     }
     
     /**
-     * SHA3-256 String hashing
+     * To prompt the user when access to KeyStore is needed
+     * @param passwordInput
+     * @return 
+     */
+    public boolean verifyKSPassword(String passwordInput) {
+        char[] tempPassInput = computeHash(passwordInput);
+        return Arrays.equals(tempPassInput, ksHashedPassword);
+        
+    }
+    
+    /**
+     * SHA3-256 String hashing.
      *
      * @param message
-     * @param algo
      * @return
      */
-    public byte[] computeHash(String message, String algo) {
+    private char[] computeHash(String message) {
+        char[] hash = null;
         MessageDigest messageDigest = null;
         try {
-            messageDigest = MessageDigest.getInstance(algo);
+            messageDigest = MessageDigest.getInstance("SHA3-256");
         } catch (NoSuchAlgorithmException e) {
             e.printStackTrace();
         }
-
-        return messageDigest.digest(message.getBytes(StandardCharsets.UTF_8));
+        byte[] bytes = messageDigest.digest(message.getBytes(StandardCharsets.UTF_8));
+        Charset charset = Charset.forName("UTF-8");
+        String temp = new String(bytes, charset);
+        hash = temp.toCharArray();
+        return hash;
     }
     
    // public void encryptFile(String algo, SecretKey secretKey, )
@@ -107,7 +138,7 @@ public class CryptoUtility {
             try {
                 FileOutputStream fsOutput = new FileOutputStream("src\\main\\resources\\keystore.p12");
 
-                ks.store(fsOutput, ksPassword);
+                ks.store(fsOutput, ksHashedPassword);
                 System.out.println("Key stored");
             } catch (KeyStoreException | FileNotFoundException | NoSuchAlgorithmException
                                        | CertificateException e) {
